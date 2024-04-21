479b80605db82f8f4d1b9ddede1480fe10a4d061
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2024-04-21 20:12:29 -0300

    amazing monorepo

--- Teste/src/main.cpp
+++ Teste/src/main.cpp
@@ -1,5 +1,6 @@
+#include<GL\glew.h>
 #include<GLFW/glfw3.h>
-
+#include<glm\glm.hpp>
 
 int main()
 {
@@ -11,10 +12,11 @@ int main()
 	window = glfwCreateWindow(1024, 768, "Teste OpenGL", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
+	glClearColor(0.1f, 0.2f, 0.6f, 0.0f);
 
 	while(!glfwWindowShouldClose(window))
 	{
-
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
