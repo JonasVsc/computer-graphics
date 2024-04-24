34d1eac22e27c57aa3576e192af99be1821b9bbb
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2024-04-24 09:45:54 -0300

    model view projection again

--- SnakeGame/src/main.cpp
+++ SnakeGame/src/main.cpp
@@ -36,13 +36,36 @@ int main()
 		// view       -> *
 		// projection -> *
 
+		// model
+		// -----
 		glm::mat4 model = glm::mat4(1.0f);
 		model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 		model = glm::scale(model, glm::vec3(0.2, 0.2, 0.2));
-		
 
+		// model-location
+		// --------------
 		GLuint model_location = glGetUniformLocation(my_shader.program, "model");
 		glUniformMatrix4fv(model_location, 1, GL_FALSE, glm::value_ptr(model));
+
+		// view
+		// ----
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+		// view-location
+		// -------------
+		GLuint view_location = glGetUniformLocation(my_shader.program, "view");
+		glUniformMatrix4fv(view_location, 1, GL_FALSE, glm::value_ptr(view));
+
+		// projection
+		// ----------
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(45.0f), SCREEN_WIDTH / SCREEN_HEIGHT, 0.1f, 100.0f);
+
+		// projection-location
+		// -------------------
+		GLuint projection_location = glGetUniformLocation(my_shader.program, "projection");
+		glUniformMatrix4fv(projection_location, 1, GL_FALSE, glm::value_ptr(projection));
 
 
 		// render
